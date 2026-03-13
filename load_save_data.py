c921157c36d8081c7c698992b133f48311628744
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2026-03-13 18:00:58 -0400

    modify loaddataset

--- load_save_data.py
+++ load_save_data.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 import config as cfg
 # Charger le split complet
-ds = load_dataset("jhu-clsp/kreyol-mt", "hat-eng")
+ds = load_dataset("jhu-clsp/kreyol-mt", "hat-eng", download_mode="force_redownload")
 
 print(ds)
 ds.save_to_disk(cfg.save_path)
