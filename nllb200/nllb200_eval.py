f157f7d4eccfb61e1d3eced1d6e0a2a23e41ce90
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2026-03-19 01:28:44 -0400

    m

--- nllb200/nllb200_eval.py
+++ nllb200/nllb200_eval.py
@@ -30,7 +30,11 @@ test_ds  = ds["test"]
 model_path = "nllb200_baseline/checkpoint-41565"
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(
+    model,
+    src_lang="hat_Latn",
+    tgt_lang="eng_Latn"
+)
 
 
 # --------------------------------
