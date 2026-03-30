d750da400665942df524356690c91129a875d86c
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2026-03-30 19:23:42 -0400

    evaluation

--- nllb200/nllb200_eval.py
+++ nllb200/nllb200_eval.py
@@ -18,7 +18,7 @@ test_ds = ds["test"]
 # -------------------------------
 # Chargement du modèle et tokenizer
 # -------------------------------
-model_path = "nllb200_baseline4/checkpoint-98000"
+model_path = "nllb200_baseline4/checkpoint-166260"
 base_model = "facebook/nllb-200-distilled-600M"
 
 SRC_LANG = "hat_Latn"
