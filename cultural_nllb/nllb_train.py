4e5daed48d723af1b63d1a819681e488d4466f81
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2026-05-06 00:03:21 -0400

    hh

--- cultural_nllb/nllb_train.py
+++ cultural_nllb/nllb_train.py
@@ -118,8 +118,8 @@ training_args = Seq2SeqTrainingArguments(
     per_device_train_batch_size=4,
     per_device_eval_batch_size=4,
 
-    learning_rate=1e-5,
-    num_train_epochs=1,
+    learning_rate=5e-6,
+    num_train_epochs=2,
 
     eval_strategy="epoch",
     save_strategy="epoch",
