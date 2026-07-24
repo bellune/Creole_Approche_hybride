4c5a495be7d9f38fe18b621f0945f3249f5d4867
Bellune Tabitha Megane <bellunetabithamegane@gmail.com>
2026-07-24 17:52:06 -0400

    evaluattion shared task

--- Shared_task2026/nllb200_eval_EN_all.py
+++ Shared_task2026/nllb200_eval_EN_all.py
@@ -237,7 +237,7 @@ print("Baseline BLEURT :", baseline_bleurt["scores"][0])
 df_scores = pd.DataFrame({
     "cr": sources,
     "reference_en": refs,
-    "direction": "hat-eng",
+    "direction": ["hat-eng"] * len(sources),
     "Model": ["NLLB Baseline", "NLLB (CS)"],
     "BLEU": [baseline_bleu["score"]],
     "chrF": [baseline_chrf["score"]],
