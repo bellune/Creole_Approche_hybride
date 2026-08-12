3b8bf46179bb3a5634ed05903df6e60786c5c3e5
Bellune Tabitha <bellunetabithamegane@gmail.com>
2026-08-12 05:53:42 +0000

    train

--- fairseq/train_eval.py
+++ fairseq/train_eval.py
@@ -163,7 +163,7 @@ def train_transformer():
       --keep-best-checkpoints 1 \
       --best-checkpoint-metric loss \
       --distributed-world-size 1 \
-      --ddp-backend no_c10d
+      --fp16 --ddp-backend no_c10d
     """
 
     run_cmd(cmd)
